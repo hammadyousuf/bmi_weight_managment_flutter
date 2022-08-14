378eec66d82d23016ddebcad01b5cbfe2b581605
hammad <hammadbhat126@gmail.com>
2022-08-14 10:16:32 +0400

    separate pages statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bmi_weight_managment_flutter/Imput_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -12,28 +13,6 @@ class BMICalculator extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         ),
       home: InputPage(),
-    );
-  }
-}
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text('BMI CALCULATOR'),
-
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
